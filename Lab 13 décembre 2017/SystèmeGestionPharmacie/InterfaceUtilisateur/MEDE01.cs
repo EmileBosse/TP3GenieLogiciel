9aac825428eb18abec150ee852967791c0892a30
Émile Bossé <emile.bosse@cidco.ca>
2017-12-17 13:51:41 -0500

    recherche medecin

--- Lab 13 décembre 2017/SystèmeGestionPharmacie/InterfaceUtilisateur/MEDE01.cs
+++ Lab 13 décembre 2017/SystèmeGestionPharmacie/InterfaceUtilisateur/MEDE01.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using SystèmeGestionPharmacie.Logique;
+using SystèmeGestionPharmacie.DAL.MSSQL;
 using SystèmeGestionPharmacie.InterfaceUtilisateur;
 
 namespace SystèmeGestionPharmacie
@@ -28,6 +30,25 @@ namespace SystèmeGestionPharmacie
         private void btnAjouterMedecin_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void btnRechercher_Click(object sender, EventArgs e)
+        {
+            lbMedecins.Items.Clear();
+            MédecinMapper mM = new MédecinMapper();
+            List<Médecin> ms = mM.Find(txtRechercheNumero.Text, txtRechercheNom.Text, txtRecherchePrenom.Text);
+
+            if (ms != null)
+            {
+                for (int i = 0; i < ms.Count; i++)
+                {
+                    lbMedecins.Items.Add(ms[i].Nom+" "+ms[i].Prénom);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Aucun résultat.");
+            }
         }
     }
 }
